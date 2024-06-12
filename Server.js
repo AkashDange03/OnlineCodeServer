462493578cb21aa6848ef5e678c2c727aa4a0ed0
Akash_03 <akash.dange0307@gmail.com>
2024-06-12 14:52:33 +0530

    port evn

--- Server.js
+++ Server.js
@@ -89,7 +89,7 @@ io.on("connection", (socket) => {
         socket.leave();
     })
 })
-
-server.listen(5000, () => {
-    console.log("server running on port 5000")
+const port = process.env.PORT || 5000
+server.listen(port, () => {
+    console.log(`server running on port ${port}`);
 })
